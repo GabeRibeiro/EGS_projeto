fd0f4720bee5045f000e575afadd0f01a20fe819
GabeRibeiro <gabrielribeiro00@ua.pt>
2022-05-11 11:09:27 +0000

    authentication

--- authentication_service/index.js
+++ authentication_service/index.js
@@ -2,24 +2,107 @@ const express = require('express')
 const dotenv = require('dotenv')
 const app = express()
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const User = require('./model/User')
+const jwt = require('jsonwebtoken')
+const verify = require('./routes/verifyToken')
+const { registerValidation, loginValidation } = require('./validation')
+const url = 'mongodb://127.0.0.1:27017';
 
-//Import routes
-const authRoute = require('./routes/auth')
+app.set('view-engine', 'ejs')
+
+
 
 dotenv.config();
 
 //Connect to DB
 mongoose.connect(
-    process.env.DB_CONNECT 
-    ,{useNewUrlParser: true}
-    ,() => console.log('connected to mongodb'))
-
+        url , {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }, (err, client) => {
+        if (err) {
+            return console.log(err);
+        }
+    
+        console.log('connected to mongodb:' + url);
+    }); 
 
 //Middleware
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-//Route middlewares
-app.use('/api/user',authRoute)
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/register', async (req, res) => {
+    //Validate
+    const { error } = registerValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message) 
+
+    //Check if user already exists
+    const emailExists = await User.findOne({email: req.body.email})
+    if(emailExists) return res.status(400).send('Email already exists')
+
+
+    //Hash passwords
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+    //Create user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    })
+
+    try {
+        const savedUser = await user.save()
+        res.send({user: user._id})
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+//LOGIN
+app.post('/login', async (req, res) => {
+    //Validate
+    const { error } = loginValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message) 
+
+    //Check if user exists
+    const user = await User.findOne({email: req.body.email})
+    if(!user) return res.status(400).send('Email wrong')
+
+    //Check if password is correct
+    const validPW = await bcrypt.compare(req.body.password, user.password)
+    if(!validPW) return res.status(400).send('Password wrong')
+    
+    //Create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET, {expiresIn: '6h'})
+    res.header('auth-token', token).send({user: user._id}).status(200)
+    
+})
+
+
+
+app.post('/verifyToken', verify,  async (req,res) => {
+
+    res.json({
+        message: 'Sucessfully logged in',
+    })
+    res.status(200)
+    
+    
+})
+
+
 
 app.listen(3000, () => console.log('Server running'))
 
